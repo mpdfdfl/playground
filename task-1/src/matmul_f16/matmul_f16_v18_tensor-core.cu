35b2d7bd7df3b162534e51b2ca9e10ea55406c21
mpdfdfl <2899278823@qq.com>
2025-06-09 11:51:05 +0000

    mma优化到了175

--- task-1/src/matmul_f16/matmul_f16_v18_tensor-core.cu
+++ task-1/src/matmul_f16/matmul_f16_v18_tensor-core.cu
@@ -2,27 +2,28 @@
 // #include <cuda_runtime.h>
 // #include <library_types.h>
 
-// #include "../xmx/fp16/fp16_v17.cuh"
+// #include "../xmx/fp16/fp16_v18.cuh"
 // #include "playground/matmul.hpp"
 // #include "playground/static.hpp"
 // #include "playground/system.hpp"
 // #include <cstdio>
 // namespace playground
 // {
-// PLAYGROUND_MATMUL_DEC(float16_t, 17, M, N, K, A, B, C)
+// PLAYGROUND_MATMUL_DEC(float16_t, 18, M, N, K, A, B, C)
 // {
 
 //     dim3 dimBlock(32, 2, 4);
 //     dim3 dimGrid(N / 128, M / 256);
-//     size_t smem_size = (128 * 32 + 256 * 32) * sizeof(float16_t) * 4;
+//     size_t smem_size = max((128 * 32 + 256 * 32) * sizeof(float16_t) * 4,
+//                            (256 * 128) * sizeof(float16_t));
 //     cudaError_t err = cudaGetLastError();
 //     if (err != cudaSuccess) {
 //         printf("CUDA Error before kernel: %s\n", cudaGetErrorString(err));
 //     }
-//     cudaFuncSetAttribute(gemm_fp16_v17,
+//     cudaFuncSetAttribute(gemm_fp16_v18,
 //                          cudaFuncAttributeMaxDynamicSharedMemorySize,
 //                          131072);
-//     gemm_fp16_v17<<<dimGrid, dimBlock, smem_size>>>(A, B, C, M, N, K);
+//     gemm_fp16_v18<<<dimGrid, dimBlock, smem_size>>>(A, B, C, M, N, K);
 
 //     err = cudaDeviceSynchronize();
 //     if (err != cudaSuccess) {
